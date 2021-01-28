4ba3e8f130e5c50543ded7b30df13dc517945872
edwardboucher <edbouch@yahoo.com>
2021-01-28 17:43:28 -0500

    attempt 05

---
+++ ntp_state/ntp_module/ntpmodule_example.py
@@ -0,0 +1,50 @@
+import logging
+try:
+    import requests
+    HAS_REQUESTS = True
+except ImportError:
+    HAS_REQUESTS = False
+
+log = logging.getLogger(__name__)
+
+__virtual_name__ = 'ntpmodule'
+
+def __virtual__():
+    '''
+    Only load ntpmodule if requests package is available
+    '''
+    if HAS_REQUESTS:
+        return __virtual_name__
+    else:
+        return False, 'The ntpmodule module cannot be loaded: requests package unavailable, check pip.'
+
+
+def get(signs=None):
+    '''
+    Gets the Current NTP state of salt nodes
+
+    CLI Example::
+
+        salt minion weather.get KPHL
+
+    This module also accepts multiple values in a comma seperated list::
+
+        salt minion weather.get KPHL,KACY
+    '''
+    log.debug(signs)
+    return_value = {}
+    signs = signs.split(',')
+    for sign in signs:
+        return_value[sign] = _make_request(sign)
+    return return_value
+
+def _make_request(sign):
+    '''
+    The function that makes the request for weather data from the National Weather Service.
+    '''
+    request = requests.get('https://api.weather.gov/stations/{}/observations/current'.format(sign))
+    conditions = {
+        "description:": request.json()["properties"]["textDescription"],
+        "temperature": round(request.json()["properties"]["temperature"]["value"], 1)
+    }
+    return conditions
