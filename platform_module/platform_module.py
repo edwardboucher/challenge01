9a27da1ce00150efbc2a05c5ff0292e143abdaae
Edward Boucher <eboucher@sunayu-mac-6t.local>
2021-03-27 21:15:22 -0400

    subprocess module added 04

--- platform_module/platform_module.py
+++ platform_module/platform_module.py
@@ -2,14 +2,24 @@ import platform
 import subprocess
 
 try:
-    import platform
+import platform
+
     HAS_OS = True
 except ImportError:
     HAS_OS = False
 
-#log = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 __virtual_name__ = 'platform_module'
+
+def __virtual__():
+    '''
+    Only load platform_module if os package is available
+    '''
+    if HAS_OS:
+        return __virtual_name__
+    else:
+        return False, 'The platform_module module cannot be loaded: os package unavailable, check pip.'
 
 def get():
     return_value = {}
