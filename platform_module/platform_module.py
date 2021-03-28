86dcb19be39cfe5ef3db738e83fb022632fe2eae
Edward Boucher <eboucher@sunayu-mac-6t.local>
2021-03-27 21:21:20 -0400

    subprocess module added 05

--- platform_module/platform_module.py
+++ platform_module/platform_module.py
@@ -13,13 +13,16 @@ log = logging.getLogger(__name__)
 __virtual_name__ = 'platform_module'
 
 def __virtual__():
-    '''
-    Only load platform_module if os package is available
-    '''
+    """
+    only load cheese if platform module is available
+    """
     if HAS_OS:
         return __virtual_name__
     else:
-        return False, 'The platform_module module cannot be loaded: os package unavailable, check pip.'
+        return (
+            False,
+            'The platform_module module cannot be loaded: os package unavailable, check pip.',
+        )
 
 def get():
     return_value = {}
