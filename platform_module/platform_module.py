deb39d9d0ec428e107f2e5bf3716ea1fce51df1a
Edward Boucher <eboucher@sunayu-mac-6t.local>
2021-03-27 20:35:37 -0400

    playform module

---
+++ platform_module/platform_module.py
@@ -0,0 +1,29 @@
+import platform
+
+try:
+import platform
+    HAS_OS = True
+except ImportError:
+    HAS_OS = False
+
+log = logging.getLogger(__name__)
+
+__virtual_name__ = 'platform_module'
+
+def __virtual__():
+    '''
+    Only load platform_module if os package is available
+    '''
+    if HAS_OS:
+        return __virtual_name__
+    else:
+        return False, 'The platform_module module cannot be loaded: os package unavailable, check pip.'
+
+def get():
+    return_value = {}
+    output = subprocess.check_output("platform.dist()",shell=True)
+    return output
+
+def _interrogate_service():
+    output = subprocess.check_output("platform.dist()",shell=True)
+    return output
